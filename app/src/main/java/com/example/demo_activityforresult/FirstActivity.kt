a9a6e25ef5b0fbdd1d1a8e071b8669b296b3bd4d
Beratcan Güreş <guresberatcan@gmail.com>
2021-05-23 19:27:23 +0300

    FirstActivity finished

--- app/src/main/java/com/example/demo_activityforresult/FirstActivity.kt
+++ app/src/main/java/com/example/demo_activityforresult/FirstActivity.kt
@@ -1,11 +1,20 @@
 package com.example.demo_activityforresult
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class FirstActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first)
+
+        val btn_finish = findViewById<Button>(R.id.btn_finish)
+
+        btn_finish.setOnClickListener {
+            setResult(Activity.RESULT_OK)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/demo_activityforresult/MainActivity.kt
+++ app/src/main/java/com/example/demo_activityforresult/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.example.demo_activityforresult
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private const val  FIRST_ACTIVITY_REQUEST_CODE = 1
+    }
+
+    lateinit var btn_launch_activity_first: Button
+    lateinit var btn_launch_activity_second: Button
+    lateinit var tv_first_activity_result: TextView
+    lateinit var tv_second_activity_result: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_launch_activity_first = findViewById(R.id.btn_launch_activity_first)
+        btn_launch_activity_second = findViewById(R.id.btn_launch_activity_second)
+        tv_first_activity_result = findViewById(R.id.tv_first_activity_result)
+        tv_second_activity_result = findViewById(R.id.tv_second_activity_result)
+
+        btn_launch_activity_first.setOnClickListener{
+            val intent = Intent(this, FirstActivity::class.java)
+            startActivityForResult(intent, FIRST_ACTIVITY_REQUEST_CODE)
+        }
+
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode == FIRST_ACTIVITY_REQUEST_CODE){
+                tv_first_activity_result.text = "First Activity Result Success"
+            }
+        }else if(resultCode == Activity.RESULT_CANCELED){
+            Log.e("Cancelled", "Cancelled")
+            Toast.makeText(this,"Result Cancelled",Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
